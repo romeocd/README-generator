7034de823e8a95a19e60150342f70c11c6d52591
romeo <rdumlao07@gmail.com>
2023-10-24 19:58:48 -0400

    created function to write README file

--- Develop/index.js
+++ Develop/index.js
@@ -50,7 +50,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(paht.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
