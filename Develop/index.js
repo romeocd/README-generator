548fc1012a3bf409589e68cacacda96f2aebee61
romeo <rdumlao07@gmail.com>
2023-10-24 19:58:09 -0400

    created array of questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,49 @@ const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter project title.",
+    },
+    {
+        type:"input",
+        name: "description",
+        message:"Please provide short description explaining the purpose of this project.",
+    },
+    {
+        type:"input",
+        name: "installation",
+        message:"Please provide steps required to install this project.",
+    },
+    {
+        type:"input",
+        name: "usage",
+        message:"Please provide instructions and examples for use",
+    },
+    {
+        type:"list",
+        name: "license",
+        message:"Please select license for the project.",
+        choices: "None, Apache License 2.0, MIT License, GNU General Public License v3.0"
+    },
+    {
+        type:"input",
+        name: "contributing",
+        message:"Please enter contribution guidelines. ",
+    },
+    {
+        type:"input",
+        name: "tests",
+        message:"Please enter instructions on how to test this project.",
+    },
+    {
+        type:"input",
+        name: "questions",
+        message:"Please enter your GitHub username and email address for any additional questions.",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
