82183e26b9d3b30b0a970edfd147b857be3376e2
romeo <rdumlao07@gmail.com>
2023-10-24 20:28:18 -0400

    created function that returns license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  let badge = '';
+
   if(license  === "Apache License 2.0"){
-    badge = "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    badge = "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
   }
   else if(license === "MIT License") {
-    badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    badge = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
   }
   else if (license === "GNU General Public License v3.0") {
-  badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  badge = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
   }
   else {badge = ''
   }
@@ -18,7 +20,22 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseURL = '';
+    if (license === "Apache License 2.0") {
+      licenseURL = "(https://opensource.org/licenses/Apache-2.0)"
+    }
+    if (license === "MIT License") {
+      licenseURL = "(https://opensource.org/licenses/MIT)"
+    }
+    if (license === "GNU General Public License v3.0") {
+      licenseURL = "(https://www.gnu.org/licenses/gpl-3.0)"
+    } else {
+      licenseURL = ""
+    }
+    return licenseURL;
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
